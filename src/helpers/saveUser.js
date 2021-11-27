50a67d46b53d0fe2c8cee936806f12ac11b935ec
MaximoJunior <studentdeveloper19@gmail.com>
2021-11-27 11:59:50 -0500

    Saved

--- src/helpers/saveUser.js
+++ src/helpers/saveUser.js
@@ -1,11 +1,13 @@
 
 
-export const saveUser = (data) => {
+export const saveUser = async (data) => {
     const requestOptions = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
       };
 
-      return fetch("https://mmmmmmmm/s", requestOptions);
+      let data = await fetch("https://mmmmmmmm/s", requestOptions);
+      data = await data.json();
+      return data;
 }
